8ea23899a892568f2bf6fee41ea13a55cb3d4bf6
Gabriela Borba <gborbas.gb@gmail.com>
2025-03-28 11:47:09 -0300

    first commit

---
+++ src/test/java/com/os/WebOS/WebOsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.os.WebOS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebOsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
